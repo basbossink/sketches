0268afbb32a81785bc109b3b34faef9de2094adf
Bas Bossink <bas.bossink@gmail.com>
2013-07-21 02:02:11 +0200

    Add Knightrider bar and Cylon commander eyes
    - Make all contstants uppercase.
    - Reorder declarations to types, prototypes, constants, functions.

--- ledTrain/ledTrain.ino
+++ ledTrain/ledTrain.ino
@@ -1,8 +1,10 @@
 enum TRAIN_STATE {
   OFF,
+  KNIGHT_RIDER,
+  CYLON_COMMANDER,
   RUNNING,
-  DOUBLE_SPEED,
-  QUADRUPLE_SPEED,
+  HIGH_SPEED,
+  ULTRA_HIGH_SPEED,
   STOPPING,
   STOPPED_BEFORE_BACKWARDS,
   BACKWARDS,
@@ -13,11 +15,14 @@ enum TRAIN_STATE {
   ODD_BLINKING,
 }; 
 
+enum DIRECTION {
+  LEFT_TO_RIGHT,
+  RIGHT_TO_LEFT,
+};
+
 struct led {
   int pin;
-  int highValue;
-  int isOn;
-  void (*write)(uint32_t, uint32_t);
+  bool isOn;
 };
 
 struct state { 
@@ -37,15 +42,19 @@ void loopAllBlinkingState();
 void loopEvenBlinkingState();
 void loopOddBlinkingState();
 void switchOffAll();
-void loopDoubleSpeedRunningState();
-void loopQuadrupleSpeedRunningState();
+void loopHighSpeedRunningState();
+void loopUltraHighSpeedRunningState();
+void loopKnightRiderState();
+void loopCylonCommanderState();
+void switchOffAllAndResetLedIndex();
 
-const int numberOfStates = 12;
 const struct state STATES[] = {
-  { OFF, RUNNING, &switchOffAll, &nop },
-  { RUNNING, DOUBLE_SPEED, 0, &loopRunningState },
-  { DOUBLE_SPEED, QUADRUPLE_SPEED, 0, &loopDoubleSpeedRunningState },
-  { QUADRUPLE_SPEED, STOPPING, 0, &loopQuadrupleSpeedRunningState },
+  { OFF, KNIGHT_RIDER, &switchOffAll, &nop },
+  { KNIGHT_RIDER, CYLON_COMMANDER, &switchOffAllAndResetLedIndex, &loopKnightRiderState },
+  { CYLON_COMMANDER, RUNNING, &switchOffAllAndResetLedIndex, &loopCylonCommanderState },
+  { RUNNING, HIGH_SPEED, 0, &loopRunningState },
+  { HIGH_SPEED, ULTRA_HIGH_SPEED, 0, &loopHighSpeedRunningState },
+  { ULTRA_HIGH_SPEED, STOPPING, 0, &loopUltraHighSpeedRunningState },
   { STOPPING, STOPPED_BEFORE_BACKWARDS, 0, &loopStoppingState },
   { STOPPED_BEFORE_BACKWARDS, BACKWARDS, 0, &nop },
   { BACKWARDS, BACKWARDS_STOPPING, 0, &loopRunningBackwardState },
@@ -56,31 +65,8 @@ const struct state STATES[] = {
   { ODD_BLINKING, OFF, &switchOffAll, &loopOddBlinkingState }
 };
 
-struct state nextState(struct state currentState) {
-  for(int i = 0; i < numberOfStates; i++) {
-    if(currentState.state == STATES[i].state) {
-      for(int j = 0; j < numberOfStates; j++) {
-        if(STATES[i].nextState == STATES[j].state) {
-          Serial.print("\nchanging from state: ");
-          Serial.print(currentState.state);
-          Serial.print(", to state: ");
-          Serial.println(currentState.nextState);
-          return STATES[j];
-        }
-      }
-    }
-  }
-}
+#define DIGITAL(pin) pin, false
 
-void myAnalogWrite(uint8_t pin, uint8_t value) {
-  analogWrite(pin,value);
-}
-
-#define ANALOG(pin) pin, analogHighValue, 0, &myAnalogWrite
-#define DIGITAL(pin) pin, HIGH, 0, &digitalWrite
-
-const int analogHighValue = 255;
-const int numberOfLeds = 20;
 struct led LEDS[] = {
   { DIGITAL(23) },
   { DIGITAL(25) },
@@ -104,19 +90,48 @@ struct led LEDS[] = {
   { DIGITAL(40) },
 };
 
+#undef DIGITAL
+
+const int BUTTON_PIN = 2;
+const int NUMBER_OF_LEDS = sizeof(LEDS)/sizeof(struct led);
+const int CYLON_LEFT_START_INDEX = (NUMBER_OF_LEDS/2)-1;
+const int CYLON_RIGHT_START_INDEX = CYLON_LEFT_START_INDEX + 1;
+const int NUMBER_OF_STOPPING_BLINKS = 6;
+const int NUMBER_STATES = sizeof(STATES)/sizeof(struct state);
+const unsigned long BLINK_INTERVAL = 1 << 8;
+const unsigned long CYLON_BLINK_INTERVAL = BLINK_INTERVAL >> 1;
+const unsigned long DEBOUNCE_DELAY = 50;
+const unsigned long DECELERATION_MULTIPLIER = 3;
+const unsigned long HIGH_SPEED_BLINK_INTERVAL = BLINK_INTERVAL >> 2;
+const unsigned long ULTRA_HIGH_SPEED_BLINK_INTERVAL = BLINK_INTERVAL >> 4;
+
+enum DIRECTION cylonLeftDirection = RIGHT_TO_LEFT;
+enum DIRECTION cylonRightDirection = LEFT_TO_RIGHT;
+enum DIRECTION knightRiderDirection = LEFT_TO_RIGHT;
+int buttonPushCounter = 0;
+int buttonState = 0;
+int currentOnLedIndex = 0;
+int cylonLeftIndex = 0;
+int cylonRightIndex = 0;
+int lastButtonState = 0;
+int stoppingBlinksElapsed = 0;
+struct state currentTrainState = STATES[0];
+unsigned long currentMillis = 0;
+unsigned long lastDebounceTime = 0;
+unsigned long previousMillis = 0;
 
 void switchOff(struct led *l) {
-  l->write(l->pin, 0);
-  l->isOn = 0;
+  digitalWrite(l->pin, LOW);
+  l->isOn = false;
 }
 
 void switchOn(struct led *l) {
-  l->write(l->pin, l->highValue);
-  l->isOn = 1;
+  digitalWrite(l->pin, HIGH);
+  l->isOn = true;
 }
 
 void forEachLed(void(*fn)(struct led*)) {
-  for(int i = 0; i < numberOfLeds; i++) {
+  for(int i = 0; i < NUMBER_OF_LEDS; i++) {
     fn(LEDS+i);
   }
 }
@@ -125,46 +140,39 @@ void switchOffAll() {
   forEachLed(&switchOff);
 }
 
-const int buttonPin = 2;
-const long blinkInterval = 1 << 8;
-const long decelerationMultiplier = 3;
-const int numberOfStoppingBlinks = 6;
-unsigned long previousMillis = 0;
-unsigned long currentMillis = 0;
-unsigned long lastDebounceTime = 0;
-const unsigned long debounceDelay = 50;
-
-int buttonPushCounter = 0;
-int buttonState = 0;
-int lastButtonState = 0;
-int currentOnLedIndex = 0;
-struct state currentTrainState = STATES[0];
-int stoppingBlinksElapsed = 0;
-
-void incrementCurrentLedIndex() {
-    currentOnLedIndex++;
-    if(currentOnLedIndex == numberOfLeds) {
-      currentOnLedIndex = 0;
-    }
+void resetIndex(int& index) {
+  index = 0;
 }
 
-void decrementCurrentLedIndex() {
-    currentOnLedIndex--;
-    if(currentOnLedIndex == -1) {
-      currentOnLedIndex = numberOfLeds - 1;
-    }
+void switchOffAllAndResetLedIndex() {
+  switchOffAll();
+  resetIndex(currentOnLedIndex);
 }
 
-void switchOnNextLed() {
-    switchOff(LEDS + currentOnLedIndex);
-    incrementCurrentLedIndex();
-    switchOn(LEDS + currentOnLedIndex);
+void incrementIndex(int& index) {
+  index++;
+  if(index == NUMBER_OF_LEDS) {
+    resetIndex(index);
+  }
 }
 
-void switchOnPreviousLed() {
-    switchOff(LEDS + currentOnLedIndex);
-    decrementCurrentLedIndex();
-    switchOn(LEDS + currentOnLedIndex);
+void decrementIndex(int& index) {
+  index--;
+  if(index == -1) {
+    index = NUMBER_OF_LEDS - 1;
+  }
+}
+
+void switchOnNextLed(int& index) {
+  switchOff(LEDS + index);
+  incrementIndex(index);
+  switchOn(LEDS + index);
+}
+
+void switchOnPreviousLed(int& index) {
+  switchOff(LEDS + index);
+  decrementIndex(index);
+  switchOn(LEDS + index);
 }
 
 int interValElapsed(int interval) {
@@ -177,10 +185,11 @@ int interValElapsed(int interval) {
 }
 
 void loopRunningInADirectionState(
-  void(*swithToNextLight)(), 
+  void(*swithToNextLight)(int&),
+  int& index,
   long blinkInterval) {
   if(interValElapsed(blinkInterval)) {
-    swithToNextLight();
+    swithToNextLight(index);
   }
 }
 
@@ -195,7 +204,7 @@ void toggleLed(struct led* l) {
 }
 
 void loopAllBlinkingState() {
-  if(interValElapsed(blinkInterval)) {
+  if(interValElapsed(BLINK_INTERVAL)) {
     forEachLed(&toggleLed);
   }  
 }
@@ -209,8 +218,8 @@ int isEven(int integer) {
 }
 
 void loopBlinkingState(int(*predicate)(int)) {
-  if(interValElapsed(blinkInterval)) {
-     for(int i = 0; i < numberOfLeds; i++) {
+  if(interValElapsed(BLINK_INTERVAL)) {
+     for(int i = 0; i < NUMBER_OF_LEDS; i++) {
        if(predicate(i)) {
          toggleLed(LEDS + i);
        } else {
@@ -229,24 +238,52 @@ void loopEvenBlinkingState() {
 }
 
 void loopRunningState() {
-  loopRunningInADirectionState(&switchOnNextLed, blinkInterval);
+  loopRunningInADirectionState(
+    &switchOnNextLed,
+    currentOnLedIndex,
+    BLINK_INTERVAL);
 }
 
-void loopDoubleSpeedRunningState() {
-  loopRunningInADirectionState(&switchOnNextLed, blinkInterval >> 2);
+void loopHighSpeedRunningState() {
+  loopRunningInADirectionState(
+    &switchOnNextLed, 
+    currentOnLedIndex,
+    HIGH_SPEED_BLINK_INTERVAL);
 }
-void loopQuadrupleSpeedRunningState() {
-  loopRunningInADirectionState(&switchOnNextLed, blinkInterval >> 4);
+
+void loopUltraHighSpeedRunningState() {
+  loopRunningInADirectionState(
+    &switchOnNextLed, 
+    currentOnLedIndex,
+    ULTRA_HIGH_SPEED_BLINK_INTERVAL);
 }
+
 void loopRunningBackwardState() {
-  loopRunningInADirectionState(&switchOnPreviousLed, blinkInterval);
+  loopRunningInADirectionState(
+    &switchOnPreviousLed,
+    currentOnLedIndex,
+    BLINK_INTERVAL);
 }
 
-void loopStoppingInADirectionState(void(*switchToNextLight)()) {
-  if(interValElapsed(blinkInterval + (stoppingBlinksElapsed * blinkInterval))) {
+void loopHighSpeedRunningBackwardState() {
+  loopRunningInADirectionState(
+    &switchOnPreviousLed, 
+    currentOnLedIndex,
+    HIGH_SPEED_BLINK_INTERVAL);
+}
+
+void loopUltraHighSpeedRunningBackwardState() {
+  loopRunningInADirectionState(
+    &switchOnPreviousLed, 
+    currentOnLedIndex,
+    ULTRA_HIGH_SPEED_BLINK_INTERVAL);
+}
+
+void loopStoppingInADirectionState(void(*switchToNextLight)(int&), int& index) {
+  if(interValElapsed(BLINK_INTERVAL + (stoppingBlinksElapsed * BLINK_INTERVAL))) {
     stoppingBlinksElapsed++;
-    switchToNextLight();
-    if (stoppingBlinksElapsed >= numberOfStoppingBlinks) {
+    switchToNextLight(index);
+    if (stoppingBlinksElapsed >= NUMBER_OF_STOPPING_BLINKS) {
       stoppingBlinksElapsed = 0;
       currentTrainState = nextState(currentTrainState);
     }
@@ -254,11 +291,93 @@ void loopStoppingInADirectionState(void(*switchToNextLight)()) {
 }
 
 void loopStoppingState() {
-    loopStoppingInADirectionState(&switchOnNextLed);
+  loopStoppingInADirectionState(&switchOnNextLed, currentOnLedIndex);
 }
 
 void loopStoppingBackwardState() {
-    loopStoppingInADirectionState(&switchOnPreviousLed);
+  loopStoppingInADirectionState(&switchOnPreviousLed, currentOnLedIndex);
+}
+
+void loopKnightRiderState() {
+  if(currentOnLedIndex == 0) {
+    knightRiderDirection = LEFT_TO_RIGHT;
+  }
+  if(currentOnLedIndex == NUMBER_OF_LEDS -1) {
+    knightRiderDirection = RIGHT_TO_LEFT;
+  }
+  if(knightRiderDirection == LEFT_TO_RIGHT) {
+    loopHighSpeedRunningState();
+  }
+  if(knightRiderDirection == RIGHT_TO_LEFT) {
+    loopHighSpeedRunningBackwardState();
+  }
+}
+
+void advanceLeftLed() {
+  if(cylonLeftIndex == 0) {
+    cylonLeftDirection = LEFT_TO_RIGHT;  
+  }
+  if(cylonLeftIndex == CYLON_LEFT_START_INDEX) {
+    cylonLeftDirection = RIGHT_TO_LEFT;
+  }
+  if(cylonLeftDirection == LEFT_TO_RIGHT) {
+    switchOnNextLed(cylonLeftIndex);
+  }
+  if(cylonLeftDirection == RIGHT_TO_LEFT) {
+    switchOnPreviousLed(cylonLeftIndex);
+  }
+}
+
+void advanceRightLed() {
+  if(cylonRightIndex == CYLON_RIGHT_START_INDEX) {
+    cylonRightDirection = LEFT_TO_RIGHT;  
+  }
+  if(cylonRightIndex == NUMBER_OF_LEDS -1) {
+    cylonRightDirection = RIGHT_TO_LEFT;
+  }
+  if(cylonRightDirection == LEFT_TO_RIGHT) {
+    switchOnNextLed(cylonRightIndex);
+  }
+  if(cylonRightDirection == RIGHT_TO_LEFT) {
+    switchOnPreviousLed(cylonRightIndex);
+  }
+}
+
+bool allLedsAreOff() {
+  bool returnValue = true;
+  for(int i = 0; i < NUMBER_OF_LEDS && returnValue; i++) {
+    if(LEDS[i].isOn) {
+      returnValue = false;
+    }
+  }
+  return returnValue;
+}
+
+void loopCylonCommanderState() {
+  if(allLedsAreOff()) {
+    cylonLeftIndex = CYLON_LEFT_START_INDEX;    
+    cylonRightIndex = CYLON_RIGHT_START_INDEX;
+  }
+  if(interValElapsed(CYLON_BLINK_INTERVAL)) {
+    advanceLeftLed();
+    advanceRightLed();
+  }
+}
+
+struct state nextState(struct state currentState) {
+  for(int i = 0; i < NUMBER_STATES; i++) {
+    if(currentState.state == STATES[i].state) {
+      for(int j = 0; j < NUMBER_STATES; j++) {
+        if(STATES[i].nextState == STATES[j].state) {
+          Serial.print("\nchanging from state: ");
+          Serial.print(currentState.state);
+          Serial.print(", to state: ");
+          Serial.println(currentState.nextState);
+          return STATES[j];
+        }
+      }
+    }
+  }
 }
 
 void setPinModeToOutput(struct led* l) {
@@ -267,17 +386,17 @@ void setPinModeToOutput(struct led* l) {
 
 void setup() {
   Serial.begin(9600);
-  pinMode(buttonPin, INPUT);
+  pinMode(BUTTON_PIN, INPUT);
   forEachLed(&setPinModeToOutput);
   switchOffAll();
 } 
 
 void loop() {
-  int reading = digitalRead(buttonPin);
+  int reading = digitalRead(BUTTON_PIN);
   if(reading != lastButtonState) {
     lastDebounceTime = millis();
   }
-  if((millis() - lastDebounceTime) > debounceDelay) { 
+  if((millis() - lastDebounceTime) > DEBOUNCE_DELAY) { 
     if(reading != buttonState) {
       buttonState = reading;
       if(buttonState == HIGH) {
